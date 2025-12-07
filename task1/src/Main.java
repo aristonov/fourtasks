6bdbbc2c31ee942899971636eb9b28538f642259
aristonov <klementegarsia@gmail.com>
2025-12-07 23:48:41 +0300

    Updated

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -1,24 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // проверка наших аргументов
-        if (args.length != 4) {
-            System.out.println("Нужно передать 4 параметра, пример: 6 3 5 4");
-            return;
-        }
+        int n1, m1, n2, m2;
 
-        // получаем наши параметры
-        int n1 = Integer.parseInt(args[0]);
-        int m1 = Integer.parseInt(args[1]);
-        int n2 = Integer.parseInt(args[2]);
-        int m2 = Integer.parseInt(args[3]);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите 4 положительных числа через пробел, пример: 6 3 5 4");
+        String line = scan.nextLine();
+        String[] parts = line.trim().split("\\s+");
+
+        n1 = Integer.parseInt(parts[0]);
+        m1 = Integer.parseInt(parts[1]);
+        n2 = Integer.parseInt(parts[2]);
+        m2 = Integer.parseInt(parts[3]);
 
         String path1 = buildPath(n1, m1); // путь 1 массива
         String path2 = buildPath(n2, m2); // путь 2 массива
 
         System.out.println(path1 + path2); // соединяем пути
-
     }
 
     // Наш метод для построения пути
